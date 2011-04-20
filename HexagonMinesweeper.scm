79e4bb92f9444388b4f2b904729455fbeafbd174
Karsten König <kkoenig@posteo.de>
2011-04-20 13:07:20 +0200

    updated copyright

--- HexagonMinesweeper.scm
+++ HexagonMinesweeper.scm
@@ -1,5 +1,26 @@
 #lang scheme
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; HexagonMinesweeper
+;;
+;; Written by Torben Wiechers and Karsten-Kai König
+;;
+;; Copyright (C) 2010-2011 Torben Wiechers, Karsten-Kai König <KKoenig@posteo.de>
+;;
+;; This program is free software; you can redistribute it and/or modify it under 
+;; the terms of the GNU General Public License as published by the Free Software 
+;; Foundation; either version 3 of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but WITHOUT ANY 
+;; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+;; PARTICULAR PURPOSE. See the GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License along with this 
+;; program; if not, see <http://www.gnu.org/licenses/>.
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (require (file "OO-pack.scm"))
 (require graphics/graphics)
 
